21004d578cd4fde168442129958774c983463340
Noa Udler <udlernoa4@gmail.com>
2021-11-05 16:44:14 -0700

    commit tree db6a77dbb104bbb0915d371f7896eea5ef6e237e

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+const beforeTitleCased = (element) => {
+  const sentence = element.split(" ");
+
+  for (let i = 0; i < sentence.length; i++) {
+    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].substr(1);
+  }
+
+  const joinedSentence = sentence.join(" ");
+
+  return joinedSentence;
+};
+
+function titleCased() {
+  return tutorials.map(beforeTitleCased);
 }
